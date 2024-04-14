9b4ce7f34d013ba52f70ee145365f299f0d96ff3
Yourakhalidoovic <163519051+Yourakhalidoovic@users.noreply.github.com>
2024-04-14 19:53:58 +0200

    quiz 37

---
+++ js  introduction/challenge-one.js
@@ -0,0 +1,7 @@
+let message = "There is no war in Ba Sing Se"
+'there is 10 WORDS'
+//
+let countries = ['China','Japan','South Korea','Vietnam','Malaysia']
+let capital = ['Beijing','Tokyo','Seoul','Hanoï','Kuala Lumpur']
+//
+let randomizer
